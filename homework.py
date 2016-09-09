9ad7942f8dd086e557887f71599fd87865467180
cpblack <caleb.paul.black@gmail.com>
2016-09-09 17:50:13 -0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -13,10 +13,13 @@ if minute < 10:
 
 name = raw_input("What is the name?\n")
 cost = input("What was the cost?\n")
+tax = cost * 0.07
+tip = cost * 0.15
+total = cost + tax + tip
 print str(name+":")
 print str("Meal $"+str(cost))
-print str("+Tax $"+str(cost*0.07))
-print str("+Tip $"+str(cost*0.15))
-print str("Ttl: $"+str(cost*0.22))
+print str("+Tax $"+str(tax))
+print str("+Tip $"+str(tip))
+print str("Ttl: $"+str(total))
 print str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+minute,dayhalf
-fs.save(thedate.month+"-"+thedate.day+"-"+thedate.year,name+".txt")
+fs.save(thedate.month+"-"+thedate.day+"-"+thedate.year,name+".txt",["Name: "+name,"Date: "+str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+minute,dayhalf,"Meal: $"+str(cost),"Tax: $"+str(tax)"Tip: $"+str(tip),"Total: $"+str(total)])
