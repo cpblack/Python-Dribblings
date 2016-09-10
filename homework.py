a0a58431a0fb1012a6a6d0e37652b91ef8eecd5d
cpblack <caleb.paul.black@gmail.com>
2016-09-09 22:33:39 -0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -32,7 +32,7 @@ def receipt():
 	if not os.path.exists(os.path.dirname(filename)):
 		try:
 			os.makedirs(os.path.dirname(filename))
-		except OSError as exc: # Guard against race condition
+		except OSError as exc: 
 			if exc.errno != errno.EEXIST:
             			raise
 
