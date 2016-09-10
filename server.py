031832bf192aacab1fd94ef3455d329657b7d352
cpblack <caleb.paul.black@gmail.com>
2016-09-10 15:46:56 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import fs
-items = fs.get("logs")
-if items != False and len(items) < 0:
+logFolder = "logs"
+items = fs.get(logFolder)
+if items != False and len(items) > 0:
     t = 0
     while t < len(items):
-        item = fs.readreceipt(items[t])
+        item = fs.readreceipt(logFolder+"/"+items[t])
         print(item[5])
         t = t + 1
 else:
