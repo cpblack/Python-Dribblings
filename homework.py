ed4ab00e71a153c603d3c426206e0f19a3d30d76
cpblack <caleb.paul.black@gmail.com>
2016-09-09 21:38:14 -0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -17,10 +17,10 @@ tax = cost * 0.07
 tip = cost * 0.15
 total = cost + tax + tip
 print str(name+":")
-print str("Meal $"+str(cost))
-print str("+Tax $"+str(tax))
-print str("+Tip $"+str(tip))
-print str("Ttl: $"+str(total))
+print str("Meal  $"+str(cost))
+print str("+Tax  $"+str(tax))
+print str("+Tip  $"+str(tip))
+print str("Ttl:  $"+str(total))
 printeddate = str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+minute,dayhalf
 print printeddate
 fs.save(thedate.month+"⧸"+thedate.day+"⧸"+thedate.year" "+hour+"꞉"+minute+" "+name+".txt",["Name: "+name,"Date: "+printeddate,"Meal: $"+str(cost),"Tax: $"+str(tax)"Tip: $"+str(tip),"Total: $"+str(total)])
