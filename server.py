87078d69abc579911a7f47f6ebd1495a52a21eff
cpblack <caleb.paul.black@gmail.com>
2016-09-10 16:09:15 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,19 @@
 import fs
+raw_input("Analyze Which Server?")
 logFolder = "logs"
 items = fs.get(logFolder)
-global server
-server = []
-server.append(['Jane',0.00])
-def findServer(nameIn):
-    global server
-    myOutput = False
-    t = 0
-    while t < len(server):
-        target = server[t]
-        if myOutput == False and target[0] == nameIn:
-            output = t
-        t = t + 1
 
-if items != False and len(items) > 0:
-    t = 0
-    while t < len(items):
-        item = fs.readreceipt(logFolder+"/"+items[t])
-        print(item[5])
-        t = t + 1
-    print(str(findServer('Jane')))
-else:
-    print("#Error: No logs.")
+
+def analyzeServer(nameIn):
+        serverTotal = 0.0
+    if items != False and len(items) > 0:
+        t = 0
+        while t < len(items):
+            item = fs.readreceipt(logFolder+"/"+items[t])
+            if item[6] == nameIn:
+                serverTotal = serverTotal + item[5]
+            t = t + 1
+    else:
+        print("#Error: No logs.")
+print str(analyzeServer('Jane'))
 raw_input("Press enter to continue...")
