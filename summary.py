1972c3d93d68c3fbebca8b3429d00233007346a9
cpblack <caleb.paul.black@gmail.com>
2016-09-10 13:40:58 -0700

    Update summary.py

--- summary.py
+++ summary.py
@@ -5,5 +5,23 @@ def readreceipt(name):
 	file = fs.read(name)
 	name = file[0]
 	name = name[6:]
-	return name
-print(readreceipt(logFolder+"/"+"9-10-2016 1.27PM Angel"))
+	date = file[1]
+	date = date[6:]
+	meal = file[2]
+	meal = meal[6:]
+	tax = file[3]
+	tax = tax[5:]
+	tip = file[4]
+	tip = tip[5:]
+	total = file[6]
+	total = total[6:]
+	return [name,date,meal,tax,tip,total]
+
+logs = [f for f in os.listdir(logFolder) if os.path.isfile(os.path.join(logFolder, f))]
+t = 0
+total = 0.0
+while t < len(logs):
+        check = readreceipt(logs[t])
+        total = total + check[4]
+        t = t + 1
+print "Gross: "+total
