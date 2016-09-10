483ecbe3ad12e065febcaa19b1319693da66bf80
cpblack <caleb.paul.black@gmail.com>
2016-09-10 14:00:36 -0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -38,14 +38,14 @@ def receipt():
 	tax = cost * 0.07
 	tip = cost * 0.15
 	total = cost + tax + tip
-	print str("-- "+name+" --")
+	print str("\n-- "+name+" --")
 	print str("Meal  $"+str(cost))
 	print str("+Tax  $"+str(tax))
 	print str("+Tip  $"+str(tip))
 	print str("Ttl:  $"+str(total))
 	printeddate = str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+str(minute)+dayhalf
 	print printeddate
-	print "================"
+	print "================\n"
 	filename = str(thedate.month)+"-"+str(thedate.day)+"-"+str(thedate.year)+" "+str(hour)+"."+str(minute)+dayhalf+" "+name
 	copynumber = 0
 	copytag = ""
