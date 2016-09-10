aeb45579967871eb4cf330d2367409fd454ea9af
cpblack <caleb.paul.black@gmail.com>
2016-09-10 12:27:15 -0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -44,7 +44,7 @@ def receipt():
 	print str("Ttl:  $"+str(total))
 	printeddate = str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+str(minute)+dayhalf
 	print printeddate
-	filename = str(thedate.month)+"⧸"+str(thedate.day)+"⧸"+str(thedate.year)+" "+str(hour)+"꞉"+str(minute)+" "+name+".txt"
+	filename = str(thedate.month)+"-"+str(thedate.day)+"-"+str(thedate.year)+" "+str(hour)+"."+str(minute)+dayhalf+" "+name+".txt"
 	fs.save(logFolder+"/"+filename,["Name: "+name,"Date: "+printeddate,"Meal: $"+str(cost),"Tax: $"+str(tax),"Tip: $"+str(tip),"Total: $"+str(total)])
 try:
 	while True:
