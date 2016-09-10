23a4c4d84959df90d63ce493e9dabd8fc8b86eed
cpblack <caleb.paul.black@gmail.com>
2016-09-10 13:31:23 -0700

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,5 +1,9 @@
 import os
 import fs
+logFolder = "logs"
 def readreceipt(name):
 	file = fs.read(name)
-	
+	name = file[0]
+	name = name[6:]
+	return name
+print(readreceipt(logFolder+"/"+"9-10-2016 1.27PM Angel"))
