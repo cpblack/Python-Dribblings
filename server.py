e98774fd3cd09722aadbd34d705b57a2e8993cf2
cpblack <caleb.paul.black@gmail.com>
2016-09-10 15:43:40 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import fs
 items = fs.get("logs")
-if items != False:
-  print items[0]
+if items != False and len(items) < 0:
+    t = 0
+    while t < len(items):
+        item = fs.readreceipt(items[t])
+        print(item[5])
+        t = t + 1
+else:
+    print("#Error: No logs.")
+raw_input("Press enter to continue...")
