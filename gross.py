7e420f15dceb12f8174621355a5d394f73109536
cpblack <caleb.paul.black@gmail.com>
2016-09-10 15:17:11 -0700

    Update gross.py

--- gross.py
+++ gross.py
@@ -4,13 +4,29 @@ logFolder = fs.read("config.txt")
 logFolder = logFolder[2]
 logFolder = logFolder[10:]
 
-
+def readreceipt(name):
+	myfile = fs.read(name)
+	name = myfile[0]
+	name = name[6:]
+	date = myfile[1]
+	date = date[6:]
+	meal = myfile[2]
+	meal = meal[7:]
+	tax = myfile[3]
+	tax = tax[6:]
+	tip = myfile[4]
+	tip = tip[6:]
+	total = myfile[5]
+	total = total[8:]
+	server = myfile[6]
+	server = server[8:]
+	return [name,date,meal,tax,tip,total,server]
 
 logs = [f for f in os.listdir(logFolder) if os.path.isfile(os.path.join(logFolder, f))]
 t = 0
 ftotal = 0.0
 while t < len(logs):
-        check = fs.readreceipt(logFolder+"/"+logs[t])
+        check = readreceipt(logFolder+"/"+logs[t])
         ftotal = ftotal + float(check[5])
         t = t + 1
 print "Gross: $"+str(ftotal)
