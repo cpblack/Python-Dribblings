7c31fa5f348123d376bae5e68136dc56b89aa8bc
cpblack <caleb.paul.black@gmail.com>
2016-09-09 21:41:13 -0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,26 +1,26 @@
 import datetime
 import fs
-thetime = datetime.datetime.now().time()
-thedate = datetime.datetime.now().date()
-hour = thetime.hour
-dayhalf = "AM"
-if hour > 12:
-	hour = hour - 12
-	dayhalf = "PM"
-minute = thetime.minute
-if minute < 10:
-	minute = "0"+str(minute)
-
-name = raw_input("What is the name?\n")
-cost = input("What was the cost?\n")
-tax = cost * 0.07
-tip = cost * 0.15
-total = cost + tax + tip
-print str(name+":")
-print str("Meal  $"+str(cost))
-print str("+Tax  $"+str(tax))
-print str("+Tip  $"+str(tip))
-print str("Ttl:  $"+str(total))
-printeddate = str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+minute,dayhalf
-print printeddate
-fs.save(thedate.month+"⧸"+thedate.day+"⧸"+thedate.year" "+hour+"꞉"+minute+" "+name+".txt",["Name: "+name,"Date: "+printeddate,"Meal: $"+str(cost),"Tax: $"+str(tax)"Tip: $"+str(tip),"Total: $"+str(total)])
+while True:
+	name = raw_input("What is the name?\n")
+	cost = input("What was the cost?\n")
+	thetime = datetime.datetime.now().time()
+	thedate = datetime.datetime.now().date()
+	hour = thetime.hour
+	dayhalf = "AM"
+	if hour > 12:
+		hour = hour - 12
+		dayhalf = "PM"
+	minute = thetime.minute
+	if minute < 10:
+		minute = "0"+str(minute)
+	tax = cost * 0.07
+	tip = cost * 0.15
+	total = cost + tax + tip
+	print str(name+":")
+	print str("Meal  $"+str(cost))
+	print str("+Tax  $"+str(tax))
+	print str("+Tip  $"+str(tip))
+	print str("Ttl:  $"+str(total))
+	printeddate = str(thedate.month)+"/"+str(thedate.day)+"/"+str(thedate.year)+", "+str(hour)+":"+minute,dayhalf
+	print printeddate
+	fs.save(thedate.month+"⧸"+thedate.day+"⧸"+thedate.year" "+hour+"꞉"+minute+" "+name+".txt",["Name: "+name,"Date: "+printeddate,"Meal: $"+str(cost),"Tax: $"+str(tax)"Tip: $"+str(tip),"Total: $"+str(total)])
