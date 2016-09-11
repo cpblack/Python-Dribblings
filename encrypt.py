a3496c13437d70f554a81e082ab071595a82a095
cpblack <caleb.paul.black@gmail.com>
2016-09-10 20:53:58 -0700

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,19 @@
+import base64
+def encrypt(stringIn,layers):
+    t = 0
+    m = stringIn
+    while len(m) % 4 > 0:
+        m = m+"."
+    while t < layers:
+        m = base64.b64encode(m)
+        t = t + 1
+    return m
+def decrypt(stringIn,layers):
+    t = 0
+    m = stringIn
+    while t < layers:
+        m = base64.b64decode(m)
+        t = t + 1
+    while m[-1:] == ".":
+        m = m[:-1]
+    return m
