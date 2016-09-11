3752116c4f364ffef246e9d78092d216970a3790
cpblack <caleb.paul.black@gmail.com>
2016-09-11 11:35:04 -0700

    Update b64.py

--- b64.py
+++ b64.py
@@ -1,5 +1,5 @@
 import base64
-def encrypt(stringIn,layers):
+def encode(stringIn,layers):
     t = 0
     m = stringIn
     while len(m) % 4 > 0:
@@ -8,7 +8,7 @@ def encrypt(stringIn,layers):
         m = base64.b64encode(m)
         t = t + 1
     return m
-def decrypt(stringIn,layers):
+def decode(stringIn,layers):
     t = 0
     m = stringIn
     while t < layers:
