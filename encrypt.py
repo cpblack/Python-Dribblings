ee80af327b0a1b16b2283ad821c474fd0ef3351d
cpblack <caleb.paul.black@gmail.com>
2016-09-10 20:56:31 -0700

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -3,7 +3,7 @@ def encrypt(stringIn,layers):
     t = 0
     m = stringIn
     while len(m) % 4 > 0:
-        m = m+"."
+        m = m+"="
     while t < layers:
         m = base64.b64encode(m)
         t = t + 1
@@ -14,6 +14,6 @@ def decrypt(stringIn,layers):
     while t < layers:
         m = base64.b64decode(m)
         t = t + 1
-    while m[-1:] == ".":
+    while m[-1:] == "=":
         m = m[:-1]
     return m
