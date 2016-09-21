37c6f32ff7e7f90478aba663de27ee61753fbd25
cpblack <caleb.paul.black@gmail.com>
2016-09-21 14:58:29 -0600

    Create wordidentify.py

---
+++ wordidentify.py
@@ -0,0 +1,21 @@
+def check(input):
+    output = "Error/Unknown"
+    if input.isalpha():
+        output = "error"
+        if len(input) == 1:
+            output = "letter"
+        else:
+            output = "word"
+    else:
+        try:
+            int(input)
+            output = "interger"
+        except:
+            try:
+                float(input)
+                output = "float"
+            except:
+                "other"
+    return output
+while True:
+    print "Type: "+check(raw_input("Please enter something.\n"))
