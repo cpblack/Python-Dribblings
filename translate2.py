7a5a816aa8fe580ac8c05e5a77a49538557df011
cpblack <caleb.paul.black@gmail.com>
2016-09-23 09:44:31 -0600

    Update translate2.py

--- translate2.py
+++ translate2.py
@@ -6,7 +6,7 @@ from re import sub as re.sub
 
 def getLanguage():
     language = "this should not be used as a language name"
-    while fs.exists("translations/"+language+".txt") == False:
+    while fs.exists("translations/"+language+".txt") == False and language.lower() != "pig latin":
         language = raw_input("Please enter the name of your language file located in the Translations folder\n")
     return 
 def getAddAll():
@@ -25,23 +25,39 @@ def smartAddAll(string1, string2):
 def translate(stringIn):
     global dictionary
     global addAll
-    stringIn = re.sub(r'([^\s\w]|_)+', '', stringIn)
-    output = ""
-    targets = stringIn.split(" ")
-    t = 0
-    while t < len(targets):
-        if targets[t] in dictionary:
-            output = output +" "+ smartAddAll(dictionary[targets[t]], addAll)
-        else:
-            output = output +" "+ smartAddAll(targets[t],addAll)
-        t = t + 1
-    output = output[1:]
+    if language.lower() != "pig latin":
+        stringIn = re.sub(r'([^\s\w]|_)+', '', stringIn)
+        output = ""
+        targets = stringIn.split(" ")
+        t = 0
+        while t < len(targets):
+            if targets[t] in dictionary:
+                output = output +" "+ dictionary[targets[t]]
+            else:
+                output = output +" "+ targets[t]
+            t = t + 1
+        output = output[1:]
+    else:
+        stringIn = re.sub(r'([^\s\w]|_)+', '', stringIn)
+        output = ""
+        targets = stringIn.split(" ")
+        t = 0
+        while t < len(targets):
+            lj = targets[t]
+            if lj[:1] in "aeiou":
+                lj = lj + "way"
+            else:
+                lj = lj[1:]+lj[:1]+"ay"
+            output = output + " " + lj
+            t = t + 1
+        output = output[1:]
     return output
 
 try:  
     global addAll
     global dictionary
-    dictionary = fs.interpret(fs.read("translations/"+getLanguage()+".txt"))
+    if language.lower != "pig latin":
+        dictionary = fs.interpret(fs.read("translations/"+getLanguage()+".txt"))
     addAll = getAddAll()
     print dictionary
     while True:
