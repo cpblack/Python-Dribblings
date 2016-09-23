a74a21f65187a7304c199e835aaa93a357831524
cpblack <caleb.paul.black@gmail.com>
2016-09-23 09:06:19 -0600

    Create translate2.py

---
+++ translate2.py
@@ -0,0 +1,20 @@
+import fs
+import re
+dictionary = fs.interpret(fs.read("translations.txt"))
+print dictionary
+
+def translate(stringIn):
+    stringIn = re.sub(r'([^\s\w]|_)+', '', stringIn)
+    output = ""
+    targets = stringIn.split(" ")
+    t = 0
+    while t < len(targets):
+        if targets[t] in dictionary:
+            output = output +" "+ dictionary[targets[t]]
+        else:
+            output = output +" "+ targets[t]
+        t = t + 1
+    output = output[1:]
+    return output
+input = raw_input("Enter a phrase to translate.\n")
+print translate(input)
