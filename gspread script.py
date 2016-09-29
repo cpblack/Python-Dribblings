19a37c020e11ab8b30b36542bb1dc793f2b06529
cpblack <caleb.paul.black@gmail.com>
2016-09-29 13:08:25 -0600

    Update gspread script.py

--- gspread script.py
+++ gspread script.py
@@ -5,8 +5,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread-april-2cd … ba4.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 
 gc = gspread.authorize(credentials)
 
 wks = gc.open("Where is the money Lebowski?").sheet1
+
+wks.update_acell('B2', "it's down there somewhere, let me take another look.")
