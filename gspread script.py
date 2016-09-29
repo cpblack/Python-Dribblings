e90fb08d91ebdaf15d9d1657e4ee2fdd229ad162
cpblack <caleb.paul.black@gmail.com>
2016-09-29 13:11:35 -0600

    Update gspread script.py

--- gspread script.py
+++ gspread script.py
@@ -9,6 +9,7 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open_by_key("1npB50U4RIShkm-DCEiwCy50fIaXajTYscdyRHh5T4hY").sheet1
+spreadsheet = gc.open_by_key("1npB50U4RIShkm-DCEiwCy50fIaXajTYscdyRHh5T4hY")
+wks = spreadsheet.sheet1
 
 wks.update_acell('B2', "it's down there somewhere, let me take another look.")
