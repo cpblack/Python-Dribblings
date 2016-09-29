e73a552493f80954680d0ea0d19f3a96a286d970
cpblack <caleb.paul.black@gmail.com>
2016-09-29 13:18:46 -0600

    Update gspread script.py

--- gspread script.py
+++ gspread script.py
@@ -2,14 +2,27 @@
 #OAUTH2 LIBRARY FOUND HERE: https://github.com/google/oauth2client
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://spreadsheets.google.com/feeds']
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-
-gc = gspread.authorize(credentials)
-
-spreadsheet = gc.open_by_key("1npB50U4RIShkm-DCEiwCy50fIaXajTYscdyRHh5T4hY")
-wks = spreadsheet.sheet1
-
-wks.update_acell('B2', "He's alive bobby!")
+def initialize():
+	global gc
+	global wks
+	global spreadsheet
+	global initialized
+	scope = ['https://spreadsheets.google.com/feeds']
+	
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+	
+	gc = gspread.authorize(credentials)
+	
+	spreadsheet = gc.open_by_key("1npB50U4RIShkm-DCEiwCy50fIaXajTYscdyRHh5T4hY")
+	wks = spreadsheet.sheet1
+	wks.update_acell('B2', "He's alive bobby!")
+	initialized = True
+def write(cell,content):
+	global wks
+	global spreadsheet
+	global initialized
+	global gc
+	while initialized != True:
+		print "Initializing."
+		initialize()
+	wks.update_acell(cell,content)
