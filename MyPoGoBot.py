857daa163b7d022376254aafc03b9b1c4ac04cdc
cpblack <caleb.paul.black@gmail.com>
2016-09-30 12:16:21 -0600

    Update MyPoGoBot.py

--- MyPoGoBot.py
+++ MyPoGoBot.py
@@ -2,14 +2,49 @@
 global loggedIn
 global api
 global settings
+global loggedIn = False
+from time import sleep
 settings = {"logintype":"google","username":"reallyisfakeIthink@gmial.com","password":"thisisnottherealpassword"}
 api = pgoapi.PGoApi()
 
 def login():
+	output = False
 	if settings.logintype != "ptc" and settings.logintype != "google":
 		print("ERROR: Invalid Login Type!")
+		output = False
 	else:
-		rawLogin(settings.logintype,settings.username,settings.password)
+		success = rawLogin(settings.logintype,settings.username,settings.password)
+		if success:
+			output = True
+			print "Login Success!"
+		else:
+			output = False
+			print "Login Failure!"
+	return output
+	
 def rawLogin(type,user,pass):
 	global api
-	api.login(type,user,pass)
+	return api.login(type,user,pass)
+def recycle():
+	req = api.create_request()
+	req.release_pokemon(pokemon_id = <your_input>)
+	req.release_pokemon(pokemon_id = <your_input>)
+	req.release_pokemon(pokemon_id = <your_input>)
+	response = req.call()
+	
+
+#########################################
+#########################################
+loginAttempts = 0
+while loggedIn == False and loginAttempts < 6:
+	loginAttempts = loginAttemps + 1
+	print "Attemping Log In"
+	loggedIn = login()
+	if loggedIn == False:
+		print "Login Failure, Sleeping!"
+		sleep(10)
+if loggedIn:
+	print "Login Success! Starting Script"
+	
+else:
+	print "Complete Login Failure!"
